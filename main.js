e8fb7e4a98793e4646d1c9fef2698c0fe167c2e2
Stefan <44169588+stefanrodic@users.noreply.github.com>
2019-05-04 18:10:34 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,111 @@
-// Select all cards and convert node list to array
-// add event listener
-
-if (document.readyState == 'loading') {
-    document.addEventListener('DOMContentLoaded', ready);
-} else {
-    ready();
-}
+let duration = 30000
+const timerDisplay = document.querySelector("#timer")
+let secs = 0
 
 
-let count = 0;
-
-let matched = [];
-let openedCards = [];
 
 
-function ready() {
+const ready = () => {
+    let count = 0;
+    let matched = [];
+    let openedCards = [];
+    let score = 0;
+
+
     let cards = Array.from(document.getElementsByClassName('card'));
+
+
+
+    const handleClick = (card) => {
+
+  
+
+        if (matched.indexOf(card) >= 0 || openedCards.length == 2 || openedCards.indexOf(card) >= 0) {
+            return
+        }
+     
+        else {
+            card.classList.add('visible');
+            count++
+            document.querySelector("#counter").innerHTML = count
+            timer(30000);
+
+
+
+
+            openedCards.push(card);
+
+
+            if (openedCards.length == 2) {
+           
+                if (openedCards[0].lastElementChild.src == openedCards[1].lastElementChild.src) {
+                    matched.push(openedCards[0])
+                    matched.push(openedCards[1])
+                    updateScore()
+                    checkForV()
+                    openedCards = []
+
+
+
+
+               
+                } else {
+                    setTimeout(function () {
+                        openedCards[0].classList.remove("visible")
+                        openedCards[1].classList.remove("visible")
+                        updateScore()
+                        openedCards = []
+                    }, 1200)
+                    return
+
+                }
+
+            }
+        }
+
+
+
+    }
+   
+    const checkForV = () => {
+        if (matched.length == cards.length) {
+            let edit2 = document.querySelector(".grid-container")
+
+            edit2.innerHTML = `<div class="imgend"><img id="img-vic" src="pics/victory.png"> <div> <a href="index.html">New game </a> </div ></div>`
+
+
+            clearInterval(countdown);
+
+        }
+    }
+    
+
+    const gameLost = () => {
+        let edit = document.querySelector(".grid-container")
+
+        edit.innerHTML = `<div class="imgend"><img id="img-lost" src="pics/defeat.png">  
+          <div> <a href="index.html">New game </a> </div > </div> `
+
+
+
+
+
+    }
+
+    const newGame = () => {
+        cards.forEach(card => {
+
+            card.classList.remove("visible")
+            matched = [];
+            openedCards = [];
+            count = 0;
+            document.querySelector("#counter").innerHTML = count
+
+            ready()
+
+        });
+
+    }
 
     cards.forEach(card => {
         for (let i = cards.length - 1; i > 0; i--) {
@@ -23,37 +113,72 @@ function ready() {
             cards[randIndex].style.order = i;
             cards[i].style.order = randIndex;
         }
+        card.addEventListener('click', function mdd() { handleClick(card) })
 
-        card.addEventListener('click', () => {
-            handleClick(card)
-
-
-        });
     });
-}
 
 
-function handleClick(card) {
-    card.classList.add('visible');
-    count++
-    openedCards.push(card);
 
-    console.log(openedCards)
-    console.log(openedCards[0].type)
-    let len = openedCards.length;
-    console.log(len)
+    const timer = (duration) => {
+        if (count == 1) {
 
-    if (len == 2) {
-        if (openedCards[0].type === openedCards[1].type) {
-            openedCards[0].removeEventListener("click", handleClick)
-            openedCards[1].removeEventListener("click", handleClick)
-            matched.push(openedCards[0])
-            matched.push(openedCards[1])
-            openedCards = []
-            console.log(matched)
-        }
-        else { alert("no match") }
+            displayT(30)
+            const now = Date.now();
+            const then = now + duration
+
+            countdown = setInterval(() => {
+                const secs = Math.round((then - Date.now()) / 1000)
+
+                if (secs < 0) {
+                    clearInterval(countdown);
+                    gameLost()
+                    return
+                }
+                displayT(secs)
+            }, 1000)
+        } else { return }
+
     }
 
+
+    const displayT = (secs) => {
+        const mins = Math.floor(secs / 60);
+        const rsecs = secs % 60;
+        const display = `${mins}:${rsecs < 10 ? "0" : ""}${rsecs}`;
+        timerDisplay.textContent = display;
+
+    }
+
+    const updateScore = () => {
+        score = secs + ((matched.length) * (matched.length)) - count
+
+        let result = document.querySelector("#result")
+        result.innerHTML = score;
+        if (score > 1) {
+            result.style.color = "green";
+        }
+        else {
+            result.style.color = "rgb(202, 172, 1)";
+        }
+
+
+    }
+    return { cards, count, openedCards, matched, newGame, gameLost }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
